4e9b5a15b273f3e43cda0b39b0a8794e4e2f7128
Neal Zhao <Ruiheng.Zhao@dh.com>
2018-12-10 19:48:48 -0500

    override statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,21 +14,55 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException("Need at least 5 students.");
+            {
+                throw new InvalidOperationException("Ranked Grading need at least 5 students.");
+            }
 
             var threshold = (int) Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
             if (grades[threshold - 1] <= averageGrade)
+            {
                 return 'A';
+            }
             else if (grades[threshold * 2 - 1] <= averageGrade)
+            {
                 return 'B';
+            }
             else if (grades[threshold * 3 - 1] <= averageGrade)
+            {
                 return 'C';
+            }
             else if (grades[threshold * 4 - 1] <= averageGrade)
+            {
                 return 'D';
+            }
             else
+            {
                 return 'F';
+            }
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked Grading need at least 5 students");
+                return;
+            }
+ 
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked Grading need at least 5 students");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
         }
     }
 }
